94d001f4f787a9ffc3076d746c0ad7696e1b1422
anmoldesai <anmol.desai@lendi.com.au>
2018-07-30 12:01:52 +1000

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,9 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/, loader: 'style-loader!css-loader'
+            },
+            {
+                test: /\.js$/, loader: 'babel-loader', exclude: [/node_modules/], query: {presets: ['es2015']}
             }
         ]
     }
